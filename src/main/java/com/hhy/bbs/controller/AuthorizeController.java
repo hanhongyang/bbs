19b5799eed6641b9f4f28d00e2ae37cd9024b08a
hanhongyang <1143780280@qq.com>
2019-10-19 13:42:30 +0800

    抽到properties里

--- src/main/java/com/hhy/bbs/controller/AuthorizeController.java
+++ src/main/java/com/hhy/bbs/controller/AuthorizeController.java
@@ -1,8 +1,10 @@
 package com.hhy.bbs.controller;
 
 import com.hhy.bbs.dto.AccessTokenDTO;
+import com.hhy.bbs.dto.GithubUser;
 import com.hhy.bbs.provider.GithubProvider;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -11,15 +13,24 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class AuthorizeController {
 @Autowired
 private GithubProvider githubProvider;
+@Value("${github.client.id}")
+   private String clientId;
+@Value("${github.client.secret}")
+   private String clientSerect;
+@Value("${github.redirect.uri}")
+   private String redirectUri ;
     @GetMapping("/callback")
     public String callback(@RequestParam(name = "code")String code,
                            @RequestParam(name="state")String state){
         AccessTokenDTO accessTokenDTO=new AccessTokenDTO();
+        accessTokenDTO.setClient_id(clientId);
+        accessTokenDTO.setClient_secret(clientSerect);
         accessTokenDTO.setCode(code);
-        accessTokenDTO.setRedirect_uri("http://localhost:8080/callback");
+        accessTokenDTO.setRedirect_uri(redirectUri);
         accessTokenDTO.setState(state);
-        githubProvider.getAccessToken(accessTokenDTO);
-
+        String accessToken=githubProvider.getAccessToken(accessTokenDTO);
+        GithubUser user=githubProvider.getUser(accessToken);
+        System.out.println(user.getName());
         return "index";
     }
 }
--- src/main/java/com/hhy/bbs/provider/GithubProvider.java
+++ src/main/java/com/hhy/bbs/provider/GithubProvider.java
@@ -9,8 +9,9 @@ import org.springframework.stereotype.Component;
 import java.io.IOException;
 
 @Component
+
 public class GithubProvider {
-    public String getAccessToken(AccessTokenDTO accessTokenDTO){
+    public String getAccessToken(AccessTokenDTO accessTokenDTO) {
         MediaType mediaType = MediaType.get("application/json; charset=utf-8");
         OkHttpClient client = new OkHttpClient();
 
@@ -20,27 +21,30 @@ public class GithubProvider {
                 .post(body)
                 .build();
         try (Response response = client.newCall(request).execute()) {
-             String string=response.body().string();
-            System.out.println(string);
-            return string;
-        } catch (IOException e) {
+            String string = response.body().string();
+            String token = string.split("&")[0].split("=")[1];
+            return token;
+        } catch (Exception e) {
             e.printStackTrace();
         }
         return null;
     }
-public GithubUser getUser(String accessToken){
-    OkHttpClient client = new OkHttpClient();
-    Request request = new Request.Builder()
-            .url("https://api.github.com/user?access_token=" + accessToken)
-            .build();
-    try {
-        Response response = client.newCall(request).execute();
-        String string=response.body().string();
-        GithubUser githubUser = JSON.parseObject(string, GithubUser.class);//sting的json对象转换成githubuser对象
-        return githubUser;
-    } catch (IOException e) {
-        e.printStackTrace();
+
+
+    public GithubUser getUser(String accessToken) {
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder()
+                .url("https://api.github.com/user?access_token=" + accessToken)
+                .build();
+        try {
+            Response response = client.newCall(request).execute();
+            String string = response.body().string();
+            GithubUser githubUser = JSON.parseObject(string, GithubUser.class);
+            return githubUser;
+        } catch (Exception e) {
+            e.printStackTrace();// log.error("getUser error,{}", accessToken, e);
+        }
+        return null;
     }
-    return null;
-}
+
 }
