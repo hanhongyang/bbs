c655959c97f5e4e2aaf17ea53a438fccd7e2c65d
hanhongyang <1143780280@qq.com>
2019-10-19 14:29:49 +0800

    session登录

--- src/main/java/com/hhy/bbs/controller/AuthorizeController.java
+++ src/main/java/com/hhy/bbs/controller/AuthorizeController.java
@@ -9,6 +9,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.HttpServletRequest;
+
 @Controller
 public class AuthorizeController {
 @Autowired
@@ -21,7 +23,8 @@ private GithubProvider githubProvider;
    private String redirectUri ;
     @GetMapping("/callback")
     public String callback(@RequestParam(name = "code")String code,
-                           @RequestParam(name="state")String state){
+                           @RequestParam(name="state")String state,
+                           HttpServletRequest request){
         AccessTokenDTO accessTokenDTO=new AccessTokenDTO();
         accessTokenDTO.setClient_id(clientId);
         accessTokenDTO.setClient_secret(clientSerect);
@@ -30,7 +33,14 @@ private GithubProvider githubProvider;
         accessTokenDTO.setState(state);
         String accessToken=githubProvider.getAccessToken(accessTokenDTO);
         GithubUser user=githubProvider.getUser(accessToken);
-        System.out.println(user.getName());
-        return "index";
+        if(user!=null){
+            request.getSession().setAttribute("user",user);
+            return "redirect:/";
+            //登录成功
+        }else{
+            return "redirect:/";
+            //登陆失败
+        }
+
     }
 }
