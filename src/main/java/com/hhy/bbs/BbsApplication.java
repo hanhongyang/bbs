44b959d18c8eb1139fe1cd1b7199b1934e82db95
hanhongyang <1143780280@qq.com>
2019-10-29 21:51:57 +0800

    mybatis generator

--- src/main/java/com/hhy/bbs/BbsApplication.java
+++ src/main/java/com/hhy/bbs/BbsApplication.java
@@ -1,9 +1,11 @@
 package com.hhy.bbs;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan(basePackages = "com.hhy.bbs.mapper")
 public class BbsApplication {
 
     public static void main(String[] args) {
